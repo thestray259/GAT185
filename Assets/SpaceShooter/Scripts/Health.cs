61c922a1f0f2872bd38fd02c855783b323e3aafb
Tobie Allen <tallen@student.neumont.edu>
2022-01-19 16:06:00 -0700

    Fixed the collide issues

--- Assets/SpaceShooter/Scripts/Health.cs
+++ Assets/SpaceShooter/Scripts/Health.cs
@@ -8,31 +8,26 @@ public class Health : MonoBehaviour
     [SerializeField] bool destroyOnDeath = true;
     [SerializeField] float maxHealth = 100;
 
-    public float health; 
+    public float health { get; set; }
 
     void Start()
     {
-        health = maxHealth; 
-    }
-
-    void Update(float damage)
-    {
-        health -= damage; 
-        if (health <= 0)
-        {
-            if (deathPrefab != null)
-            {
-                Instantiate(deathPrefab, transform.position, transform.rotation); 
-            }
-            if (destroyOnDeath)
-            {
-                Destroy(gameObject); 
-            }
-        }
+        health = maxHealth;
     }
 
     public void Damage(float damage)
     {
-        Update(damage); 
+        health -= damage;
+        if (health <= 0)
+        {
+            if (deathPrefab != null)
+            {
+                Instantiate(deathPrefab, transform.position, transform.rotation);
+            }
+            if (destroyOnDeath)
+            {
+                Destroy(gameObject);
+            }
+        }
     }
 }
