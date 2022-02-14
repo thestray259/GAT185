f265d603333f56afd36ac5ab595f11f2e85499a5
Tobie Allen <tallen@student.neumont.edu>
2022-02-14 10:57:23 -0700

    Started Character game stuff

---
+++ Assets/Character/Scripts/CharacterPlayer.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+public class CharacterPlayer : MonoBehaviour
+{
+    [SerializeField] CharacterController controller;
+    [SerializeField] Animator animator; 
+    [SerializeField] float speed;
+    [SerializeField] float jumpForce;
+
+    Vector3 velocity = Vector3.zero; 
+
+    void Update()
+    {
+        if (controller.isGrounded && velocity.y < 0)
+        {
+            velocity.y = 0; 
+        }
+
+        Vector3 direction = Vector3.zero;
+
+        direction.x = Input.GetAxis("Horizontal");
+        direction.z = Input.GetAxis("Vertical");
+
+        controller.Move(direction * speed * Time.deltaTime);
+
+        animator.SetFloat("speed", controller.velocity.magnitude);
+
+        if (controller.velocity.magnitude > 0)
+        {
+            transform.forward = controller.velocity; 
+        }
+
+        if (controller.isGrounded && Input.GetButtonDown("Jump"))
+        {
+            velocity.y += jumpForce; 
+        }
+
+        velocity += Physics.gravity * Time.deltaTime; 
+        controller.Move(velocity * Time.deltaTime); 
+
+    }
+}
