f29bc05b975267f2cb80d3a8ce39674f5e722c5e
Tobie Allen <tallen@student.neumont.edu>
2022-02-24 10:49:46 -0700

    Effects assignment mostly done

---
+++ Assets/Character/Scripts/Bolt.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bolt : MonoBehaviour
+{
+    [SerializeField] LineRenderer lineRenderer;
+    [SerializeField] Transform start;
+    [SerializeField] Transform end;
+
+    [SerializeField] int segments;
+    [SerializeField] float radius;
+
+    void Update()
+    {
+        Vector3[] positions = new Vector3[segments + 1];
+
+        Vector3 direction = (end.position - start.position).normalized; //< get direction vector from end to start >;
+        float length = 1.5f;//< get length of each segment>;
+
+        positions[0] = start.position; //< set start position>;
+        positions[segments] = end.position; //< set end position>;
+
+        for (int i = 1; i < segments; i++)
+        {
+            positions[i] = start.position + direction.normalized * length; //< normalized direction* length * ???>;
+            positions[i] = positions[i] + Random.insideUnitSphere * radius; //< Random inside unit sphere* radius>;
+        }
+
+        lineRenderer.positionCount = positions.Length;
+        lineRenderer.SetPositions(positions);
+    }
+}
